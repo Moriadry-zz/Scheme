1edcad643c8829092695eb4d29326dec6bfec5bb
Moriadry <dp11@software.nju.edu.cn>
2016-06-28 10:35:15 +0800

    the constant class

---
+++ src/main/java/dp/scheme/SchemeConstants.java
@@ -0,0 +1,14 @@
+package hdm.pk070.jscheme;
+
+/**
+ *
+ */
+public class SchemeConstants {
+
+    public static final int EOF = -1;
+
+    public static final String BOOL_TRUE_VAL = "#t";
+    public static final String BOOL_FALSE_VAL = "#f";
+    public static final String NIL_VAL = "nil";
+
+}
